9e140ed8daa89fae1b98022553f613d0852e13f4
Scott Obray <sobray11@gmail.com>
2017-12-14 21:53:56 -0700

    add tweet button to quote page

--- quote.js
+++ quote.js
@@ -1,10 +1,12 @@
-
+window.onload = getQuote();
 
 function getQuote() {
   url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?"
   $.getJSON(url, function(data){
     $('#author').html("-" + data.quoteAuthor);
     $('#quote').html('"' + data.quoteText + '"');
+    $('#tweet').attr('href', 'https://twitter.com/intent/tweet?text="' + data.quoteText + '" -' + data.quoteAuthor);
+
     if(data.quoteAuthor == ""){
       $('#author').html("-Anonymous");
     }
